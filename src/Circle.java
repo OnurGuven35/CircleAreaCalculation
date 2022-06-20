b0a036cc83b3523e7346d3e59ad852dd2ade23af
OnurGuven35 <gvnn.onrr@gmail.com>
2022-06-20 13:14:13 +0300

    .

--- src/Circle.java
+++ src/Circle.java
@@ -1,18 +1,27 @@
+/* Java ile yarı çapını kullanıcıdan aldığınız dairenin alanını ve çevresini hesaplayan programı yazın.
+Alan Formülü : π * r * r;
+Çevre Formülü : 2 * π * r; */
+
 import java.util.Scanner;
 
 public class Circle {
     public static void main(String[] args) {
-
-        double r, pi = 3.14, alan, cevre;
-
         Scanner input = new Scanner(System.in);
+
+        double r, pi = 3.14, area, perim;
+
+        // Kullanıcıdan değer alıp verilen formüllere göre çevre ve alanı yazdırıyoruz.
+
+        System.out.println();
         System.out.print("Lutfen Dairenin Yaricapini Giriniz : ");
         r = input.nextDouble();
 
-        alan = pi * r * r;
-        cevre = 2 * pi * r;
+        area = pi * r * r;
+        perim = 2 * pi * r;
 
-        System.out.println("Dairenin Alani : " + alan);
-        System.out.print("Dairenin Cevresi : " + cevre);
+        System.out.println();
+        System.out.println("Dairenin Alani : " + area);
+        System.out.print("Dairenin Cevresi : " + perim);
+        System.out.println();
     }
 }
